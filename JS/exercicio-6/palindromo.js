644d06bb14d9612dfb81ce14ea76370ad1f5322c
Raphael-Albergaria <hraphael.albergaria@hotmail.com>
2023-02-19 22:49:19 -0300

    Estudo com exercicio polídromo

--- JS/exercicio-6/palindromo.js
+++ JS/exercicio-6/palindromo.js
@@ -1,5 +1,17 @@
 let palavra = prompt('Escreva uma palavra')
-
-for (; indice <= 10; indice++) {
-    alert(indice)
+let palavraInvertida = ''
+// let isPolidromo
+// for (let i = tamanhoDaPalavra; i >= 0; i--) {
+//     palavraAoContrario += palavra[i]
+// }
+// prompt(palavraAoContrario)
+for (let i = palavra.length - 1; i >= 0; i--) {
+    palavraInvertida += `${palavra[i]}`
+}
+if (palavra !== palavraInvertida) {
+    alert(`A palavra ${palavra} não é plídromo:
+    ${palavra}
+    ${palavraInvertida}`)
+} else {
+    alert(`A palavra ${palavra} é um polídromo!`)
 }
--- JS/exercicio-6/roboTabuada.js
+++ JS/exercicio-6/roboTabuada.js
@@ -1,7 +1,7 @@
-let numero = parseFloat(prompt('Qual o número da tabuada que você quer?'))
+const numero = parseFloat(prompt('Qual o número da tabuada que você quer?'))
 let tabuada = ''
 
-for (let i = 0; i <= 20; i++) {
+for (let i = 1; i <= 20; i++) {
     tabuada += `${numero} x ${i} = ${numero * i}\n`
 }
 alert(`A tabuada de ${numero}:
