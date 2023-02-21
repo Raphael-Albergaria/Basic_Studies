ae158ca9595ac6ca35a96f2a5a6e61036b776478
Raphael-Albergaria <hraphael.albergaria@hotmail.com>
2023-02-21 12:41:33 -0300

    Estudo de Array (includes, indexOf, slice, concat, splice e percorrendo array com FOR)

--- JS/aula-9/trabalhandoComArrays.js
+++ JS/aula-9/trabalhandoComArrays.js
@@ -1,4 +1,4 @@
-const arr = [
+const mesesAno = [
     'Janeiro',
     'Fevereiro',
     'Março',
@@ -11,21 +11,64 @@ const arr = [
     'Outubro',
     'Novembro'
 ]
-console.log(arr)
+console.log(mesesAno)
+//---Incluir e retirar elementos---
 
 //push - adicionar no final
-arr.push('Dezembro')
-console.log(arr)
+mesesAno.push('Dezembro')
+mesesAno.push('2023')
+console.log(mesesAno)
 
 //unshift - adicionar no inicio
-arr.unshift(2023)
-console.log(arr)
+mesesAno.unshift(2023)
+console.log(mesesAno)
 
 //pop - tira o ultimo elemento
-const ultimoElemento = arr.pop()
-console.log(arr)
+let ultimoElemento = mesesAno.pop()
+console.log(mesesAno)
 console.log(ultimoElemento)
 
 //shift - tira o primeiro elemento
-ultimoElemento = arr.shift
-console.log(arr)
+ultimoElemento = mesesAno.shift()
+console.log(mesesAno)
+console.log(ultimoElemento)
+
+//---Pesquisa---
+
+//includes - verifica se tem um determinado elemento em um array
+const inclui = mesesAno.includes('Janeiro')
+console.log(inclui)
+
+//indexOf - retorna o indice do elemento
+const indexElement = mesesAno.indexOf('Junho')
+console.log(indexElement)
+
+//---Cortar e concatenar---
+
+//slice - copia uma parte do array e salva em outro array
+//slice(start, finish)
+const primeiroSemestre = mesesAno.slice(0, 6)
+const segundoSemestre = mesesAno.slice(-6)
+const sementres = mesesAno.slice(0)
+
+console.log(primeiroSemestre)
+console.log(segundoSemestre)
+console.log(sementres)
+
+// concat - junta dois arrays em um novo array
+const todosOsMeses = [2023].concat(primeiroSemestre, segundoSemestre)
+console.log(todosOsMeses)
+
+//Substituição dos elementos
+
+//splice - remove um grupo de elementos e substitui por outro
+//.splice(indice, quantidade a ser removida, *o que ira substituir*)
+const mesSubstituido = todosOsMeses.splice(indexElement + 1, 1, 'Juno')
+console.log(mesSubstituido)
+console.log(todosOsMeses)
+
+//---Percorrer array---
+for (let index = 0; index < todosOsMeses.length; index++) {
+    const element = todosOsMeses[index]
+    console.log(`${element} se encontra na posição ${index}`)
+}
