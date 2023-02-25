71417ccfead4fae23a70a2ca92c02b37dcce62f1
Raphael-Albergaria <hraphael.albergaria@hotmail.com>
2023-02-25 17:46:21 -0300

    Começãndo os estudo de DOM-Document Object Model

---
+++ JS DOM/aula-1/index.js
@@ -0,0 +1,21 @@
+
+function onMouseEnter() {
+    console.log('Entrada do mouse')
+
+}
+function onChange() {
+    console.log('Mudança de valor')
+
+}
+function onFocus() {
+    console.log('Esta em foco')
+
+
+}
+function onBlur() {
+    console.log('Saida do foco')
+}
+function onClick() {
+    console.log('Clicou no botão')
+}
+
